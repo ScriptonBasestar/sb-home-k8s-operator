5acc4df0181fe5b9a2a56c8256da78d6a4fb69c2
CEE <archmagece@users.noreply.github.com>
2024-10-25 13:21:08 +0900

    gen: operator-sdk create api --group cache --version v1alpha1 --kind Redis --resource --controller

--- cmd/main.go
+++ cmd/main.go
@@ -153,6 +153,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Memcached")
 		os.Exit(1)
 	}
+	if err = (&cachecontroller.RedisReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Redis")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
