86aecbcd7f7366050611c1c4f69c61e6edea0208
CEE <archmagece@users.noreply.github.com>
2024-10-25 13:21:48 +0900

    gen: operator-sdk create api --group devsec --version v1alpha1 --kind Vault --resource --controller

--- cmd/main.go
+++ cmd/main.go
@@ -36,8 +36,10 @@ import (
 
 	cachev1alpha1 "github.com/scriptonbasestar/sb-home-k8s-operator/api/cache/v1alpha1"
 	corev1alpha1 "github.com/scriptonbasestar/sb-home-k8s-operator/api/core/v1alpha1"
+	devsecv1alpha1 "github.com/scriptonbasestar/sb-home-k8s-operator/api/devsec/v1alpha1"
 	cachecontroller "github.com/scriptonbasestar/sb-home-k8s-operator/internal/controller/cache"
 	corecontroller "github.com/scriptonbasestar/sb-home-k8s-operator/internal/controller/core"
+	devseccontroller "github.com/scriptonbasestar/sb-home-k8s-operator/internal/controller/devsec"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -51,6 +53,7 @@ func init() {
 
 	utilruntime.Must(corev1alpha1.AddToScheme(scheme))
 	utilruntime.Must(cachev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(devsecv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -158,6 +161,13 @@ func main() {
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Redis")
+		os.Exit(1)
+	}
+	if err = (&devseccontroller.VaultReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Vault")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
